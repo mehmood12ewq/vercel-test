f85f1c167ccdc315f3841d8701119edeca0e000d
mehmood12ewq <157206399+mehmood12ewq@users.noreply.github.com>
2024-01-20 23:58:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,39 +13,39 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("hello");
 });
-app.post("/", async (req, res) => {
-  const options = {
-    apiKey: Yny99Mqbsyxy3DuCRiRwSbr9wsukez9PjwChD8Iv1wQ0JDQIe2YFdH9HFBmxoYE2,
-    title: req.body.song,
-    artist: req.body.artist,
-    optimizeQuery: true,
-  };
-  try{
-    const song = await searchSong(options);
-      let perfectMatch = [];
+// app.post("/", async (req, res) => {
+//   const options = {
+//     apiKey: Yny99Mqbsyxy3DuCRiRwSbr9wsukez9PjwChD8Iv1wQ0JDQIe2YFdH9HFBmxoYE2,
+//     title: req.body.song,
+//     artist: req.body.artist,
+//     optimizeQuery: true,
+//   };
+//   try{
+//     const song = await searchSong(options);
+//       let perfectMatch = [];
     
-      let suggest = []; 
-      for(let i = 0; i < song.length; i++){
-        if(song[i].title.startsWith(req.body.song)){
-          console.log("perfect match song title : ", song[i].title);
-          perfectMatch.push(song[i]);
-        }else{
-          suggest.push(song[i]);
-        }
-      }
+//       let suggest = []; 
+//       for(let i = 0; i < song.length; i++){
+//         if(song[i].title.startsWith(req.body.song)){
+//           console.log("perfect match song title : ", song[i].title);
+//           perfectMatch.push(song[i]);
+//         }else{
+//           suggest.push(song[i]);
+//         }
+//       }
     
-      console.log("perfect match : ", perfectMatch);
-      let songs = perfectMatch.concat(suggest);
-      console.log("song : ");
-      console.log("song original array : ", song);
+//       console.log("perfect match : ", perfectMatch);
+//       let songs = perfectMatch.concat(suggest);
+//       console.log("song : ");
+//       console.log("song original array : ", song);
     
-      console.log("Final array: ", songs);
+//       console.log("Final array: ", songs);
       
-      res.json(songs);
-  }catch(error){
-    res.json({error: "No songs found that match your search request...."})
-  }
-});
+//       res.json(songs);
+//   }catch(error){
+//     res.json({error: "No songs found that match your search request...."})
+//   }
+// });
 
 app.post("/lyrics", async (req, res) => {
   console.log("req.body : ", req.body);
