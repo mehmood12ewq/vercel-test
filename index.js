fb160b1adf8d474ec8623a4f5e0a8b7d5fc8de74
mehmood12ewq <157206399+mehmood12ewq@users.noreply.github.com>
2024-01-21 00:10:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import axios from 'axios';
+// import axios from 'axios';
 // app.use(bodyParser.json());
 
 // app.post("/", async (req, res) => {
